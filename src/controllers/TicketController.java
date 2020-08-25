1e6b0310f2abaea0cdb0ec0e35661fc72c9a535b
Jlafratta <julian.lafratta@gmail.com>
2020-08-25 18:18:41 -0300

    .

--- src/controllers/TicketController.java
+++ src/controllers/TicketController.java
@@ -1,30 +1,8 @@
 package controllers;
 
 
-import Dao.TicketDao;
-import Models.Client;
-import Models.Product;
-import Models.Ticket;
-import Models.Vehicle;
-
 public class TicketController {
 
 
-    //SERVICE O DAO
-
-   // TicketDao ticketDao;
-
-
-   // public TicketController(TicketDao ticketDao) {
-     //   this.ticketDao = ticketDao;
-    //}
-
-    //public Ticket createTicket (String date, String time, float neto, Float totalPrice, Client client, Product product, Vehicle vehicle){
-
-      //  Ticket ticket= new Ticket(date,time,neto,totalPrice,client,product,vehicle);
-
-
-        //return ticket ; ///TicketDao.AddTicket
-    //}
 
 }
